8984cd4081d500ece6e542fe93ed0d7ca0e05e71
Victoria <toribrey03@gmail.com>
2025-12-31 18:55:50 -0500

    test commit

--- src/pages/projects.ts
+++ src/pages/projects.ts
@@ -3,7 +3,7 @@ import { fetchProjects } from "../api/projects";
 async function init() {
   try {
     const projects = await fetchProjects();
-    console.log("Projects from WP JSON:", projects);
+    console.log("projects", projects);
   } catch (err) {
     console.error("Error fetching projects:", err);
   }
